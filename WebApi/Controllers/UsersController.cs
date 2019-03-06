0457bac8457ab2d1f4aec4b022575cab784aa18b
Mayrinika <mashuliy_u@mail.ru>
2019-03-06 22:36:30 +0500

    completed WebApi GET

--- WebApi/Controllers/UsersController.cs
+++ WebApi/Controllers/UsersController.cs
@@ -1,16 +1,29 @@
 using System;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using WebApi.Models;
+using WebGame.Domain;
 
 namespace WebApi.Controllers
 {
     [Route("api/[controller]")]
     public class UsersController : Controller
     {
+        public IUserRepository Repository { get; }
+
+        public UsersController(IUserRepository repository)
+        {
+            Repository = repository;
+        }
+
         [HttpGet("{userId}")]
         public ActionResult<UserDto> GetUserById([FromRoute] Guid userId)
         {
-            throw new NotImplementedException();
+            var user = Repository.FindById(userId);
+            if (user == null)
+                return NotFound();
+            var userDto = Mapper.Map<UserDto>(user);
+            return Ok(userDto);
         }
 
         [HttpPost]
--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -1,11 +1,16 @@
+using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using WebGame.Domain;
+using WebApi.Models;
 
 namespace WebApi
 {
+
     public class Startup
     {
         public Startup(IConfiguration configuration)
@@ -21,6 +26,25 @@ namespace WebApi
             services.AddRouting(options => options.LowercaseUrls = true);
             services.AddMvc(options => { })
                 .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddSingleton<IUserRepository, InMemoryUserRepository>();
+            services.AddMvc(options =>
+            {
+                // Этот OutputFormatter позволяет возвращать данные в XML, если требуется.
+                options.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter());
+                // Эта настройка позволяет отвечать кодом 406 Not Acceptable на запросы неизвестных форматов.
+                options.ReturnHttpNotAcceptable = true;
+            });
+            Mapper.Initialize(cfg =>
+          {
+                // Регистрация преобразования UserEntity в UserDto с дополнительным правилом.
+                // Также поля и свойства с совпадающими именами будут скопировны (поведение по умолчанию).
+                cfg.CreateMap<UserEntity, UserDto>()
+                  .ForMember(dest => dest.FullName, opt => opt.MapFrom(src => $"{src.LastName} {src.FirstName}"));
+
+                // Регистрация преобразования UserToUpdateDto в UserEntity без дополнительных правил.
+                // Все поля и свойства с совпадающими именами будут скопировны (поведение по умолчанию).
+                // cfg.CreateMap<UserToUpdateDto, UserEntity>();
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
